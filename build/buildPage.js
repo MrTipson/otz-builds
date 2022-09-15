44eab2dc96fb8c01225b0893554584a328ce8e41
MrTipson <gunfight7.gf@gmail.com>
2022-09-15 14:51:24 +0200

    Initial commit for page generation

---
+++ build/buildPage.js
@@ -0,0 +1,77 @@
+const url = "https://docs.google.com/spreadsheets/d/1uk0OnioNZgLly_Y9pZ1o0p3qYS9-mpknkv3DlkXAxGA/export?format=csv&gid=1886110215";
+
+const axios = require("axios").default;
+const fs = require("fs");
+const hbs = require("handlebars");
+const template = hbs.compile(fs.readFileSync("pageTemplate.hbs", { encoding: 'utf8', flag: 'r' }));
+const Papa = require("papaparse");
+const stringSimilarity = require("string-similarity");
+
+const perks = JSON.parse(fs.readFileSync("../json/perks.json"));
+for (let i = 0; i < perks.length; i++) perks[i].id = i;
+
+function parseData(callback) {
+	// Fetch builds spreadsheet
+	axios.get(url)
+		.then(data => {
+			// Parse csv file
+			Papa.parse(data.data, {
+				complete: function (result) {
+					// console.log(result);
+					result = result.data;
+					let ret = {}
+					// Grab change date
+					ret.lastUpdate = result[3][2];
+					ret.killers = [];
+					// Parse all killers
+					for (let i = 10; i < result.length; i += 13) {
+						ret.killers.push(parseKiller(result, i, 1));
+					}
+					callback(ret);
+				}
+			});
+		});
+
+}
+
+// Function that parses each individual killer
+// Data: parsed csv file as 2D array
+// Row, col: indexes of killer's name
+// All other values are retrieved based on offsets from the name
+function parseKiller(data, row, col) {
+	let killer = {};
+	killer.name = data[row][col];
+	killer.builds = [];
+	// Loop over columns (builds)
+	for (let i = 1; i < 8; i += 2) {
+		let build = {};
+		build.name = data[row + 2][col + i];
+		build.perks = [];
+		// Loop over rows (perks)
+		for (let j = 4; j < 8; j++) {
+			let perkid = findPerk(data[row + j][col + i]);
+			build.perks.push(perks[perkid]);
+		}
+		killer.builds.push(build);
+	}
+	return killer;
+}
+
+// Binary search
+function findPerk(perkName) {
+	let lower = 0;
+	let upper = perks.length - 1;
+	while (lower <= upper) {
+		let mid = Math.floor((lower + upper) / 2);
+		if (stringSimilarity.compareTwoStrings(perkName, perks[mid].name) > 0.85) {
+			return mid;
+		} else if (perks[mid].name > perkName) {
+			upper = mid - 1;
+		} else {
+			lower = mid + 1;
+		}
+	}
+	return -1;
+}
+
+parseData(data => fs.writeFileSync("../index.html", template({ killers: data.killers, changeDate: data.lastUpdate })));
---
+++ build/getPerks.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { JSDOM } = require('jsdom');
+
+(async function () {
+	// Grab webpage
+	const dom = await JSDOM.fromURL("https://deadbydaylight.fandom.com/wiki/Killer_Perks");
+	const { document } = dom.window;
+	// Remove mini icons next to links
+	document.querySelectorAll(".formattedPerkDesc")
+		.forEach((x) => x.querySelectorAll("span[style*=padding]")
+			.forEach((y) => y.remove()));
+	// First element is apparently thead (jsdom is wack)
+	// Grab all rows in table
+	perks = [...document.querySelector("tbody").children].slice(1)
+		.map((x) => {
+			// Remap each row into object
+			return {
+				filename: x.querySelectorAll("a")[0].href.replace(/\/revision\/latest.+/, ""),
+				name: x.querySelectorAll("a")[1].title,
+				// Description is URI encoded for simplicity
+				desc: encodeURI(x.querySelector(".formattedPerkDesc").innerHTML.replaceAll("/wiki/", "https://deadbydaylight.fandom.com/wiki/"))
+			}
+		});
+	// Sort so binary search can be used
+	perks.sort(function (a, b) { return a.name.localeCompare(b.name, 'en') });
+	// Write back into file
+	fs.writeFileSync("json/perks.json", JSON.stringify(perks, null, '\t'));
+}());
